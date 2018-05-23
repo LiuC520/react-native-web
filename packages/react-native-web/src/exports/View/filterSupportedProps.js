7b24773dcda7ae2fd669bdff012a6f3f41c60b96
Nicolas Gallagher <nicolasgallagher@gmail.com>
2018-05-23 16:47:08 -0700

    [change] whitelist props passed to React DOM
    Avoiding unknown property warnings from React DOM. Many components
    within and build upon React Native do not filter their own properties
    and spread them into View. It is expected that React Native ignore these
    properties.
    
    Fix #898

---
+++ packages/react-native-web/src/exports/View/filterSupportedProps.js
@@ -0,0 +1,69 @@
+const whitelist = {
+  accessibilityComponentType: true,
+  accessibilityLabel: true,
+  accessibilityLiveRegion: true,
+  accessibilityRole: true,
+  accessibilityTraits: true,
+  accessible: true,
+  children: true,
+  disabled: true,
+  importantForAccessibility: true,
+  onBlur: true,
+  onContextMenu: true,
+  onFocus: true,
+  onResponderGrant: true,
+  onResponderMove: true,
+  onResponderReject: true,
+  onResponderRelease: true,
+  onResponderTerminate: true,
+  onResponderTerminationRequest: true,
+  onStartShouldSetResponder: true,
+  onStartShouldSetResponderCapture: true,
+  onMoveShouldSetResponder: true,
+  onMoveShouldSetResponderCapture: true,
+  onTouchCancel: true,
+  onTouchCancelCapture: true,
+  onTouchEnd: true,
+  onTouchEndCapture: true,
+  onTouchMove: true,
+  onTouchMoveCapture: true,
+  onTouchStart: true,
+  onTouchStartCapture: true,
+  pointerEvents: true,
+  style: true,
+  testID: true,
+  // escape-hatches for ScrollView
+  onScroll: true,
+  onWheel: true,
+  // escape-hatches for Touchable keyboard support
+  onKeyDown: true,
+  onKeyPress: true,
+  onKeyUp: true,
+  // escape-hatches for creating hover effects
+  onMouseEnter: true,
+  onMouseLeave: true,
+  onMouseMove: true,
+  onMouseOver: true,
+  onMouseOut: true,
+  // unstable escape-hatches for web
+  className: true,
+  href: true,
+  onClick: true,
+  onClickCapture: true,
+  rel: true,
+  target: true
+};
+
+const filterSupportedProps = props => {
+  const safeProps = {};
+  for (const prop in props) {
+    if (props.hasOwnProperty(prop)) {
+      if (whitelist[prop]) {
+        safeProps[prop] = props[prop];
+      }
+    }
+  }
+  return safeProps;
+};
+
+export default filterSupportedProps;
